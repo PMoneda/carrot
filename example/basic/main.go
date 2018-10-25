89db849743a093fb9021da3862061f6e4e53cfa9
Philippe Moneda <philippemoneda@gmail.com>
2018-10-25 19:26:35 -0300

    make better management connections with rabbitmq #1

--- example/basic/main.go
+++ example/basic/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/PMoneda/carrot"
+import (
+	"fmt"
+	"time"
+
+	"github.com/PMoneda/carrot"
+)
 
 func main() {
 	config := carrot.ConnectionConfig{
@@ -20,6 +25,7 @@ func main() {
 
 	subConn, _ := carrot.NewBrokerClient(&config)
 	subscriber := carrot.NewSubscriber(subConn)
+	subscriber.SetMaxRetries(30)
 	subscriber.Subscribe(carrot.SubscribeWorker{
 		Queue: queue,
 		Scale: 1,
@@ -27,8 +33,17 @@ func main() {
 			return msg.Ack()
 		},
 	})
-
 	pub := carrot.NewPublisher(conn)
+	go func() {
+		for {
 
-	pub.Publish(exchange, "*", carrot.Message{ContentType: "application/json", Encoding: "utf-8", Data: []byte("hello")})
+			err := pub.Publish(exchange, "*", carrot.Message{ContentType: "application/json", Encoding: "utf-8", Data: []byte("hello")})
+			if err != nil {
+				fmt.Println(err)
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	ch := make(chan int)
+	<-ch
 }
